e8896c0943c63f38643f2cbf3612e9db5f06e92e
Dinno <tahadlrb7@gmail.com>
2024-04-10 20:06:59 +0330

    feat(middleware): add helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.setGlobalPrefix('/api/v1');
   await app.listen(3000);
 }
