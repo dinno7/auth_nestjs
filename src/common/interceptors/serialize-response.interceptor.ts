0e64cb33afe2f0b9cb8cebfd5bc48bfea6c51ac5
Dinno <tahadlrb7@gmail.com>
2024-04-13 19:20:27 +0330

    style(minor): remove status code in all response object

--- src/common/interceptors/serialize-response.interceptor.ts
+++ src/common/interceptors/serialize-response.interceptor.ts
@@ -1,6 +1,5 @@
 import { Injectable, NestInterceptor, StreamableFile } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Response } from 'express';
 import { ZodValidationException, validate } from 'nestjs-zod';
 import { map } from 'rxjs';
 import { SET_SERIALIZE_DTO_DECORATOR_KEY } from '../decorator/serialize.decorator';
@@ -10,14 +9,12 @@ const ErrorHandle = (e) => new ZodValidationException(e);
 export class SerializeResponseInterceptor implements NestInterceptor {
   constructor(private readonly reflector: Reflector) {}
   intercept(context, next) {
-    const res = <Response>context.switchToHttp().getResponse();
     const routeSerializeSchema = this.getContextResponseSchema(context);
 
     return next.handle().pipe(
       map((data) => {
         const result = {
           ok: true,
-          status: res.statusCode,
           timestamp: new Date().getTime(),
           data,
         };
