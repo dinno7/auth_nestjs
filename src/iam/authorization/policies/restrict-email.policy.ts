167347b0e062be089506d5e3da563d23d13efdfe
Dinno <tahadlrb7@gmail.com>
2024-04-16 16:49:43 +0330

    style(policy): add restrict email policy error message

--- src/iam/authorization/policies/restrict-email.policy.ts
+++ src/iam/authorization/policies/restrict-email.policy.ts
@@ -1,9 +1,15 @@
+import { ForbiddenException } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PolicyHandlerClass } from '../types/policy-handler.type';
 
 export class RestrictEmailPolicy implements PolicyHandlerClass {
   constructor(private __domain: string = 'gmail.com') {}
   handle(user: User): boolean | Promise<boolean> {
-    return user?.email?.endsWith(`@${this.__domain}`);
+    const isValidEmailDomain = user?.email?.endsWith(`@${this.__domain}`);
+    if (!isValidEmailDomain)
+      throw new ForbiddenException(
+        `Your email domain is not allowed, it's should be <@${this.__domain}>`,
+      );
+    return true;
   }
 }
